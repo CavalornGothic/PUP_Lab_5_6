542277db8d7d94f565026ba803b2e6fee49411e8
Arkadiusz Stando <arkadiusz.stando@protonmail.com>
2022-06-23 13:15:52 +0200

    Dodaj pliki projektów.

---
+++ PUP56/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PUP56;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PUP56/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace PUP56.Models;
+
+public class Movie
+{
+    public bool Adult { get; set; } = false;
+    public int? Budget { get; set; }
+    public string? OriginalLanguage { get; set; }
+    public string? OriginalTitle { get; set; }
+    public string? Overview { get; set; }
+    public string? Title { get; set; }
+    public double VoteAverage { get; set; }
+    public string? ReleaseDate { get; set; }
+}
